298631938537a2d49a95a7b50c5d1f6b06f64347
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-11-04 16:01:33 +0100

    Add getItem, remove useItem
    Co-authored-by: madeleineMU24 <madeleineMU24@users.noreply.github.com>
    Co-authored-by: HafsaMU24 <HafsaMU24@users.noreply.github.com>

--- Player.h
+++ Player.h
@@ -11,13 +11,15 @@ class Player
     ~Player();
     void addItem(Item* item);
     void showItems();
-    void useItem(Item* item);
+    //void useItem(Player& player);
     void removeItem(Item* item);
     int getHealth();
+    Item* getItem(int i);
     void setHealth(int newHealth);
+    std::vector<Item*> getItems();
 
     private:
-        int health;
+        int health ;
         std::vector<Item*> items;
 };
 
